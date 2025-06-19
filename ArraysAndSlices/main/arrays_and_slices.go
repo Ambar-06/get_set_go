3f2057e78023de472262c5573c272b0dfe8c948d
Ambar <brannstrom9911@gmail.com>
2025-06-19 20:47:40 +0530

    slice in go

--- ArraysAndSlices/main/arrays_and_slices.go
+++ ArraysAndSlices/main/arrays_and_slices.go
@@ -27,4 +27,15 @@ func main() {
 	fmt.Println(len(arr2))
 	fmt.Println(cap(arr2)) // capacity
 
+	// Slice : Slices are just like windows over the array, see the example below
+	var new_ar = [...]string{"a", "b", "c", "d"} // declare an array of strings a,b,c,d
+	s1 := new_ar[1:3]                            // [include:exclude] --  // s1 is a slice which only shows the element inside the window (In this case the window is represented as ()) ["a", ("b", "c"), "d"]
+	fmt.Println("Slice1:", s1)
+	fmt.Println(len(s1)) // 2
+
+	fmt.Println(cap(s1)) // 3 Why 3?
+	// cap() gives the capacity of the slice, which is:
+	// The number of elements from the starting index of the slice (index 1) to the end of the original array.
+	// In this case: from "b" (index 1) to "d" (index 3) — total 3 elements: "b", "c", "d" → so cap(s1) == 3
+
 }
